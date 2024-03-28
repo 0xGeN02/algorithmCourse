f1c9ae33919f40c87ac8a88071c9edaea382153c
0xGeN02 <manuelmateodgl02@gmail.com>
2024-03-28 19:56:13 +0100

    algorithm testing

---
+++ kata_machine/ligma.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+    dsa: [
+        "DFSOnBST",
+        "LRU",
+        "LinearSearchList",
+        "BinarySearchList",
+        "TwoCrystalBalls",
+        "BubbleSort",
+        "SinglyLinkedList",
+        "DoublyLinkedList",
+        "Queue",
+        "Stack",
+        "ArrayList",
+        "MazeSolver",
+        "QuickSort",
+        "BTPreOrder",
+        "BTInOrder",
+        "BTPostOrder",
+        "BTBFS",
+        "CompareBinaryTrees",
+        "DFSOnBST",
+        "DFSGraphList",
+        "Trie",
+        "BFSGraphMatrix",
+        "Map",
+        "MinHeap",
+
+        //new
+        "InsertionSort",
+        "MergeSort",
+        "Queue",
+        "Stack",
+        "QuickSort",
+        "DijkstraList",
+        "PrimsList",
+    ],
+}
+
